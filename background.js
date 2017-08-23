9b76886f1ef6541f7a177fcc94bc2a66ffd01b8c
vlfedotov <fedotov.vu@gmail.com>
2017-08-23 09:33:10 +0300

    add first hardcode task

--- background.js
+++ background.js
@@ -1,1 +1,8 @@
-console.log('en event page!');
+function handleButtonClick() {
+  chrome.tabs.create({
+    // index: 0,
+    url: "https://b2bpolis.atlassian.net/browse/SCRUM-3099"
+  }, function (tab) {
+    console.log(tab);
+  });
+}
---
+++ popup/popup.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict';
+
+  var backgroundPage = chrome.extension.getBackgroundPage();
+  document.querySelector('button').addEventListener('click', function () {
+    backgroundPage.handleButtonClick();
+  });
+}());
